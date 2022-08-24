162e178e6b0147c69ca6bb8c629dd3391fa9bae5
Wellington Ideão <wellington.ideao@gmail.com>
2022-08-24 14:37:33 -0300

    Aplicando Locale - localização - nos Pipes via LOCALE_ID

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
+//import '@angular/common/locales/global/pt';
+
+import { registerLocaleData } from '@angular/common';
+import localePt  from '@angular/common/locales/pt';
+import { SettingsService } from './settings.service';
+
+registerLocaleData(localePt, 'pt-BR');
 
 @NgModule({
   declarations: [
@@ -16,7 +23,17 @@ import { CamelCasePipe } from './camel-case.pipe';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+  /*{
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    }*/
+     {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (settingsService: SettingsService) => settingsService.getLocale()
+     }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
